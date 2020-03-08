c204cf8e8a6c64adb772688957b469102b5b330a
van <316089236@qq.com>
2020-03-08 16:02:49 +0800

    新建了一个mall项目

---
+++ mall/src/test/java/com/van/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.van.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
