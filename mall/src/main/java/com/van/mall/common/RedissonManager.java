73f8781a09ff7e251ef922b09bdc1f6d5a855df2
van <316089236@qq.com>
2020-03-25 20:43:10 +0800

    Redisson

--- mall/src/main/java/com/van/mall/common/RedissonManager.java
+++ mall/src/main/java/com/van/mall/common/RedissonManager.java
@@ -17,15 +17,15 @@ import javax.annotation.PostConstruct;
 public class RedissonManager {
     private Config config =new Config();
     private Redisson redisson=null;
-    private static String redisIp= PropertiesUtil.getPropertity("redis1.ip");
-    private static Integer redisPort=Integer.parseInt(PropertiesUtil.getPropertity("redis1.port")) ;
+    private  String redisIp= PropertiesUtil.getPropertity("redis1.ip");
+    private  Integer redisPort=Integer.parseInt(PropertiesUtil.getPropertity("redis1.port")) ;
 
     //构造方法完成后自动执行该方法
     @PostConstruct
     private void init(){
         try {
             //Address 是ip:port的形式
-            config.useSingleServer().setAddress(new StringBuilder().append(redisIp).append(":").append(redisPort).toString());
+            config.useSingleServer().setAddress("redis://127.0.0.1:6379");
             redisson=(Redisson) Redisson.create(config);
             log.info("初始化Redisson完成");
         } catch (Exception e) {
